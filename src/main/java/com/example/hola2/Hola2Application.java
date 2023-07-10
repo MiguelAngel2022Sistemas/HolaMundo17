6db90d04c27546d75c0386bdd04f4509ecc0c37b
MiguelAngel2022Sistemas <113484673+MiguelAngel2022Sistemas@users.noreply.github.com>
2023-07-10 01:30:04 +0000

    paso dos dockerfile

---
+++ src/main/java/com/example/hola2/Hola2Application.java
@@ -0,0 +1,13 @@
+package com.example.hola2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hola2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hola2Application.class, args);
+	}
+
+}
